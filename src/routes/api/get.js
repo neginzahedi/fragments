360a1f1b711230f52a5660e09158d3a01dd2390e
Negin Zahedi <zahedi.negin@gmail.com>
2023-06-12 11:53:39 -0400

    todo removed

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -5,7 +5,6 @@ const { createSuccessResponse } = require('../../response');
  * Get a list of fragments for the current user
  */
 module.exports = (req, res) => {
-  // TODO: this is just a placeholder to get something working...
   const fragments = []; // Placeholder for the fragments data
 
   const responseData = {
